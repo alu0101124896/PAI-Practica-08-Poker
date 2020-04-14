e59e99f901b5c2580e703531d33ac10e1470f137
Sergio Tabares <alu0101124896@ull.edu.es>
2020-04-14 13:32:29 +0000

    Ejercicios P8 13:30h

--- src/ejercicio-p08.js
+++ src/ejercicio-p08.js
@@ -10,7 +10,9 @@
 "use strict";
 
 const BLACK = 'black';
+const FIVE_PX_FONT = "5px Arial";
 const AXES_LINE_WIDTH = 3;
+const FIGURES_LINE_WIDTH = 5;
 
 /**
  * @description Function that draws two coordinate axes
@@ -29,15 +31,37 @@ function drawCoordinateAxes(CONTEXT, CANVAS) {
   CONTEXT.beginPath();
   CONTEXT.strokeStyle = BLACK;
   CONTEXT.lineWidth = AXES_LINE_WIDTH;
-  CONTEXT.moveTo(CANVAS.height/2, 0);
-  CONTEXT.lineTo(CANVAS.height/2, CANVAS.width);
+  CONTEXT.moveTo(CANVAS.height / 2, 0);
+  CONTEXT.lineTo(CANVAS.height / 2, CANVAS.width);
   CONTEXT.stroke();
+}
 
-  CONTEXT.translate(CANVAS.width / 2, CANVAS.height / 2);
+/**
+ * @description Function that draws a circle at the first cuadrant
+ *
+ * @param {*} CONTEXT - Canvas context
+ * @param {*} CANVAS - Canvas
+ */
+function drawCircle(CONTEXT, CANVAS) {
+  CONTEXT.beginPath();
+  CONTEXT.strokeStyle = BLACK;
+  CONTEXT.lineWidth = FIGURES_LINE_WIDTH;
+  CONTEXT.ellipse(CANVAS.width / 4, -CANVAS.height / 4, (CANVAS.width / 4) * 0.6, (CANVAS.height / 4) * 0.6, 0, 0, 2 * Math.PI);
+  CONTEXT.stroke();
+}
 
-  for (let xAxisIterator = 0; xAxisIterator < CANVAS.width / 2; xAxisIterator += 5) {
-
-  }
+/**
+ * @description Function that draws a rectangle at the second cuadrant
+ *
+ * @param {*} CONTEXT - Canvas context
+ * @param {*} CANVAS - Canvas
+ */
+function drawRectangle(CONTEXT, CANVAS) {
+  CONTEXT.beginPath();
+  CONTEXT.strokeStyle = BLACK;
+  CONTEXT.lineWidth = FIGURES_LINE_WIDTH;
+  CONTEXT.rect(-CANVAS.width * 2.5 / 6, -CANVAS.width * 2 / 6, CANVAS.width * 2 / 6, CANVAS.width / 6);
+  CONTEXT.stroke();
 }
 
 /**
@@ -51,5 +75,8 @@ function main() {
     CANVAS.height = window.innerHeight - 175;
 
     drawCoordinateAxes(CONTEXT, CANVAS);
+    CONTEXT.translate(CANVAS.width / 2, CANVAS.height / 2);
+    drawCircle(CONTEXT, CANVAS);
+    drawRectangle(CONTEXT, CANVAS);
   }
 }
